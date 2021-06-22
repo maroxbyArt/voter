fac68b8c9ab3a8136587a674e54304031423b85d
maroxbymassacre <maroxbymassacre@gmail.com>
2021-06-22 07:29:57 -0400

    Add hotspot layers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import introImage from 'img/study.png';
 import mapCSV from './img/home.csv';
 import mapJSON from 'img/home.json';
 import tilesSS from 'img/tiles.png';
+import playerGrid from 'img/playerGrid.png';
+import trainerSS from 'img/playerB/trainerBeta.png';
 
 import Phaser from 'phaser';
 
@@ -14,7 +16,9 @@ const height = 900;
 var map;
 var tiles;
 var layer;
+var player;
 var controls;
+var cursors;
 
 
 const config = {
@@ -23,6 +27,12 @@ const config = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 } // Top down game, so no gravity
+    }
   },
   width,
   height,
@@ -37,6 +47,10 @@ function preload() {
   this.load.image("tiles", tilesSS);
   this.load.tilemapTiledJSON('map', mapJSON);
 
+  //this.load.setPath('assets/');
+  //this.load.multiatlas('trainer', trainerSS, 'assets');
+  this.load.spritesheet('trainer', trainerSS, { frameWidth: 32, frameHeight: 32 });
+
 }
 
 
@@ -44,16 +58,20 @@ function preload() {
 function create() {
 
   map = this.make.tilemap({ key: 'map' });
-  console.log(map);
 
 
   tiles = map.addTilesetImage('base', 'tiles');
 
-  var layer = map.createStaticLayer(0, tiles, 0, 0);
+  const belowLayer = map.createStaticLayer("Background", tiles, 0, 0);
+  const worldLayer = map.createStaticLayer("World", tiles, 0, 0);
+  const hSLayer = map.createStaticLayer("Hotspots", tiles, 0, 0);
+  worldLayer.setCollisionByProperty({collides: true})
+  hSLayer.setCollisionByProperty({collides: true})
+
 
   const camera = this.cameras.main;
   // Set up the arrows to control the camera
-  const cursors = this.input.keyboard.createCursorKeys();
+  cursors = this.input.keyboard.createCursorKeys();
   controls = new Phaser.Cameras.Controls.FixedKeyControl({
     camera: camera,
     left: cursors.left,
@@ -65,11 +83,59 @@ function create() {
 
   camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
+  // === PLAYER ===
+  var config = {
+    key: 'down',
+    frames: this.anims.generateFrameNumbers('trainer', { start: 0, end: 1, first: 0 }),
+    frameRate: 1,
+    repeat: -1
+};
+
+
+this.anims.create(config);
+player = this.physics.add.sprite(400, 300, 'trainer');
+
+this.physics.add.collider(
+  player, 
+  worldLayer,
+);
+this.physics.add.collider(
+  player, 
+  hSLayer,
+  () => console.log("HIT"), 
+  null, 
+  this
+);
   
 }
 
 function update(time, delta) {
   // Apply the controls to the camera each update tick of the game
   controls.update(delta);
+
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
+
+  // Stop any previous movement from the last frame
+  player.body.setVelocity(0);
+
+  // Horizontal movement
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-100);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(100);
+  }
+
+  // Vertical movement
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-100);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(100);
+  }
+
+  // Normalize and scale the velocity so that player can't move faster along a diagonal
+  player.body.velocity.normalize().scale(speed);
+
+
 }
 
